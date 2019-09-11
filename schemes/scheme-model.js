7fbdea4a6cac529718bfc51bd134a32715a81c42
Blake <bpro0821@gmail.com>
2019-09-11 15:30:34 -0500

    adds db-config, prepares db helper methods

---
+++ schemes/scheme-model.js
@@ -0,0 +1,34 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    update,
+    remove
+}
+
+function find() {
+    return db() 
+}
+
+function findById() {
+    return db() 
+}
+
+function findSteps() {
+    return db() 
+}
+
+function add() {
+    return db() 
+}
+
+function update() {
+    return db() 
+}
+
+function remove() {
+    return db() 
+}
